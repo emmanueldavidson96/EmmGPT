393ced10975e00ca3716144ee25815ab82c2ff59
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-24 10:00:12 +0100

    First Stage Deployment

--- app/page.js
+++ app/page.js
@@ -1,113 +1,83 @@
+"use client"
 import Image from "next/image";
+import React, {useState} from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("")
+  const [chatHistory, setChatHistory] = useState([]);
+  const surpriseOptions = [
+    "Who won the latest novel prize?",
+    "what is nigeria about?",
+    "what does BLM mean?"
+  ]
+
+  const surpriseChoice = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() *surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getResponse = async () => {
+    console.log(value)
+    try{
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          history: chatHistory,
+          message:value
+        }),
+        headers:{
+          "Content-Type": "application/json"
+        }
+      }
+      const response = await fetch("http://localhost:5000/gemini", options)
+      const data = await response.text()
+      console.log(data)
+      setChatHistory(oldChatHistory => [...oldChatHistory, {
+        role: "user",
+        parts: [{text:value}]
+      },
+      {
+        role: "model",
+        parts: [{text:data}]
+      }
+    
+    ])
+    setValue("")
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="w-full h-fit pb-24 flex items-center justify-center flex-col gap-12 pt-16">
+      {/* <div className="flex w-[80%] m-auto h-[50px]">
+        <input type="text" placeholder="What do you want to know about?" className="w-[70%] text-2xl font-bold h-full text-white p-3 bg-black border-b-2 border-white" disabled />
+        <button className="bg-none text-2xl font-bold text-white bg-blue-600 w-[30%]" onClick={surpriseChoice}>Surprise Me</button>
+      </div> */}
+      <h1 className="text-white font-bold text-4xl">EMM<span className="text-green-400">GPT</span></h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flex w-[80%] m-auto h-[60px] shadow-2xl">
+        <input 
+          type="text"
+          placeholder="..."
+          className="w-[70%] text-2xl font-normal h-full p-6 bg-white rounded-tl-lg rounded-bl-lg "
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <button className="bg-none text-2xl font-bold text-white bg-green-400 w-[30%] rounded-tr-md rounded-br-md" onClick={getResponse} >
+          Ask Me
+        </button>
+      </div> 
+      <div className="w-[80%] m-auto h-fit flex flex-col gap-3">
+        {
+          chatHistory.map((chatItem, index) => (
+            <div key={index} className="bg-white p-4 mb-1">
+              <p className="text-black text-lg"><span className="font-bold capitalize">{chatItem.role}:</span> {chatItem.parts[0].text}</p>
+            </div>
+          ))
+        }      
+      </div>     
     </main>
   );
 }
